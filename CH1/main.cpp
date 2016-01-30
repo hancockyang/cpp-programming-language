f38fbd2a808aec1a76875dab7ba856d578455ed6
hankangyang <hancock.yang@gmail.com>
2016-01-30 13:50:53 -0500

    update main.cpp

--- CH1/main.cpp
+++ CH1/main.cpp
@@ -7,11 +7,12 @@
 //
 
 #include <iostream>
+#include <complex> //include complex value
+#include <vector>
+#include <cmath>
 using namespace std;
-double square(double x)
-{
-    return x*x;
-}
+
+constexpr double square(double x) {return x*x;}
 
 void print_square(double x)
 {
@@ -19,16 +20,30 @@ void print_square(double x)
 }
 
 void some_function(){
-    double d =2.2;
-    int i =7;
+    double d =2.2;                            //define double
+    double d1{2.2};                           //define double
+    vector<int> v{1,2,3,4,5};                 //define vector
+    int i =7;                                 //define integer
     d = d + i;
-    i = d*i;
+    i = d1*i;
+    complex<double> z={1,2};                  //define complex number
+    const int dmv = 17;
+    int var = 17;
+    constexpr double max1 = 1.4*square(dmv); // Ok if square(17) is constant expression
+                                             // constexpr double max2 = 1.4*square(var); error
+    const double max3 = 1.4*square(var);     // Ok, may be evaluated at run time
+    cout<<"complex number v = "<<z<<"\n";
+    cout<<"v[1] = "<<v[1]<<"\n";
+    cout<<"max1 = "<<max1<<"\n";
+    cout<<"max3 = "<<max3<<"\n";
+    
 }
 
 int main() {
     // insert code here...
     cout << "Hello, World!\n";
     print_square(1.234);
+    some_function();
     return 0;
 }
 
